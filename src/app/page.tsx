43d7973a2ac295265c1d7405dc9ace7cac03a2ed
Roy Lopez <roylopezdev@gmail.com>
2024-03-13 17:34:39 -0300

    Add navigation bar to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <nav className="border border-gray-400"></nav>
+    </main>
   );
 }
