0257e50a0430685ff92bd115306bb30fb589c337
Roy Lopez <roylopezdev@gmail.com>
2024-03-19 08:36:06 -0300

    Add header with navigation bar and reservation button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,28 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-      <nav className="border border-gray-400"></nav>
+      <header className="flex h-[143px] w-full flex-row border-2 border-gray-200">
+        <h1 className="font-rm text-4xl text-black">Paella</h1>
+        <nav className="flex-rw flex w-full grow items-center justify-between">
+          <ul className="font-rd text-dark  flex flex-row space-x-4">
+            <li>
+              <a href="#">Inicio</a>
+            </li>
+            <li>
+              <a href="#">Nosotros</a>
+            </li>
+            <li>
+              <a href="#">Menu</a>
+            </li>
+            <li>
+              <a href="#">Contacto</a>
+            </li>
+          </ul>
+          <button className="bg-accent rounded-sm border-2 px-8 py-2">
+            Reservas
+          </button>
+        </nav>
+      </header>
     </main>
   );
 }
